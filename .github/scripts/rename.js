3fc8ba89f563cb35de11d4c492f6c53ef3f57f13
Nowaaru <zackyboy135@gmail.com>
2022-08-19 16:47:51 -0700

    shit

--- .github/scripts/rename.js
+++ .github/scripts/rename.js
@@ -7,6 +7,6 @@ const toml = require("toml");
 const filePath = process.argv[2].replace(/\\/g, "/")
 // Prefix the last part of the path with "bruh" to make it easier to find
 const tomlFile = toml.parse(readFileSync(path.join(process.cwd(), "Cargo.toml"), "utf8"));
-const newFilePath = path.join(path.dirname(filePath), `spotify-np-v${tomlFile.package.version}.${path.basename(filePath)}`);
+const newFilePath = path.join(path.dirname(filePath), `spotify-np.v${tomlFile.package.version}-${path.basename(filePath)}`);
 
 renameSync(filePath, newFilePath);
